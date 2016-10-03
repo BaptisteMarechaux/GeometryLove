5f80b589c79edc30e32e9a905ce5a77e87ad2459
Manon Maillard <ravonyxa@gmail.com>
2016-10-03 16:34:41 +0200

    draw triangle

--- GeomertyLove/GeomertyLove.cpp
+++ GeomertyLove/GeomertyLove.cpp
@@ -1,6 +1,5 @@
 // GeomertyLove.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
 #include"imgui\imgui.h"
 #include "imgui_impl_glfw_gl3.h"
@@ -9,6 +8,16 @@
 #include <glfw\include\GLFW\glfw3.h>
 
 #include "ShaderManager.h"
+#include "glm.hpp"
+#include "gtc\matrix_transform.hpp"
+#include "gtc\type_ptr.hpp"
+
+GLFWwindow* window;
+GLuint vertex_buffer, vao;
+GLint mvp_location, position_location, color_location, program;
+int width, height;
+
+void Initialize();
 
 static void error_callback(int error, const char* description)
 {
@@ -17,41 +26,28 @@ static void error_callback(int error, const char* description)
 
 void Render()
 {
+	glm::mat4x4 model, proj, mvp;
 
+	model = glm::mat4(1.0);
+	proj = glm::ortho(0.0f, float(width), 0.0f, float(height), 0.0f, 100.0f);
+	mvp = proj * model;
+
+	glUseProgram(program);
+	glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
 }
 
 int main(int, char**)
 {
-	// Setup window
-	glfwSetErrorCallback(error_callback);
-	if (!glfwInit())
-		return 1;
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#if __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Geometry Love", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	gl3wInit();
-
-	// Setup ImGui binding
-	ImGui_ImplGlfwGL3_Init(window, true);
-
-	// Load Fonts
-	// (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
-	//ImGuiIO& io = ImGui::GetIO();
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
-	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-
 	bool show_test_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImColor(114, 144, 154);
+
+	Initialize();
+	glEnable(GL_CULL_FACE);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
@@ -95,9 +91,7 @@ int main(int, char**)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		Render();
-
 		ImGui::Render();
-		
 		glfwSwapBuffers(window);
 	}
 
@@ -108,3 +102,59 @@ int main(int, char**)
     return 0;
 }
 
+void Initialize()
+{
+	// Setup window
+	width = 1280;
+	height = 720;
+
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#if __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	window = glfwCreateWindow(width, height, "Geometry Love", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	gl3wInit();
+
+	// Setup ImGui binding
+	ImGui_ImplGlfwGL3_Init(window, true);
+
+	// Load Fonts
+	// (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+	//ImGuiIO& io = ImGui::GetIO();
+	//io.Fonts->AddFontDefault();
+	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
+	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+
+	//glfwSetKeyCallback(window, key_callback);
+	program = LoadShaders("..\\shaders\\simple.vs", "..\\shaders\\simple.fs");
+
+	GLfloat vertices[] = {
+		width/2, height, 0.0f,
+		0.0f, 0.0, 0.0f,
+		width, 0, 0.0f,
+	};
+
+	mvp_location = glGetUniformLocation(program, "MVP");
+	position_location = glGetAttribLocation(program, "position");
+	//color_location = glGetAttribLocation(program, "color_in");
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(float), vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(position_location);
+	glVertexAttribPointer(position_location, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindVertexArray(0);
+}
