5bd03c49355658a7cef0ac8830527abf506f5a15
Manon Maillard <ravonyxa@gmail.com>
2016-10-05 15:30:57 +0200

    begin ui

--- GeomertyLove/GeomertyLove.cpp
+++ GeomertyLove/GeomertyLove.cpp
@@ -21,6 +21,10 @@ GLuint vertexBufferPoints, vertexBufferHull, vaoPoints, vaoHull;
 GLint mvp_location, position_location, color_location, program;
 int width, height;
 int index = 0, indexHull = 0;
+int nbFrames = 0;
+double lastTime = 0.0f;
+static bool drawPoints = true;
+static bool jarvisMarchEnable = true;
 
 void Initialize();
 
@@ -47,7 +51,7 @@ void Render()
 		glDrawArrays(GL_POINTS, 0, index);
 		glBindVertexArray(0);
 	}
-	if (hull.size() > 0)
+	if (hull.size() > 0 && jarvisMarchEnable)
 	{
 		glBindVertexArray(vaoHull);
 		glDrawArrays(GL_LINE_LOOP, 0, hull.size());
@@ -59,7 +63,7 @@ void callbackMousePos(GLFWwindow *window, int button, int action, int mods)
 {
 	double x, y;
 	glfwGetCursorPos(window, &x, &y);
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS && drawPoints && ImGui::IsMouseHoveringAnyWindow() == 0)
 	{
 		y = height - y;
 		std::cout << "Point " << x << " " << y << std::endl;
@@ -77,7 +81,6 @@ void callbackMousePos(GLFWwindow *window, int button, int action, int mods)
 		glBufferData(GL_ARRAY_BUFFER, hull.size() * sizeof(Point), hull.data(), GL_STATIC_DRAW);
 
 		glBindVertexArray(0);
-		
 	}
 }
 
@@ -96,28 +99,26 @@ int main(int, char**)
 		glfwPollEvents();
 		ImGui_ImplGlfwGL3_NewFrame();
 
-		// 1. Show a simple window
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
-		{
-			static float f = 0.0f;
-			ImGui::Text("Hello, world!");
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-			ImGui::ColorEdit3("clear color", (float*)&clear_color);
-			//if (ImGui::Button("Test Window")) show_test_window ^= 1;
-			if (ImGui::Button("Another Window")) show_another_window ^= 1;
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
+		ImGui::Begin("Convex Hull & Voronoi");
+		ImGui::Text("This window is here to use the application!");
+		ImGui::Checkbox("Draw Points", &drawPoints);
+		ImGui::Checkbox("Jarvis March", &jarvisMarchEnable);
+		ImGui::ColorEdit3("clear color", (float*)&clear_color);
+		if (ImGui::Button("Test Window")) show_test_window ^= 1;
+		ImGui::End();
 
-		// 2. Show another simple window, this time using an explicit Begin/End pair
-		if (show_another_window)
+		static bool p_open = true;
+		ImGui::SetNextWindowPos(ImVec2(10, 10));
+		if (!ImGui::Begin("Example: Fixed Overlay", &p_open, ImVec2(0, 0), 0.3f, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings))
 		{
-			ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello");
 			ImGui::End();
+			return 0;
 		}
+		ImGui::Text("FPS Counter");
+		ImGui::Separator();
+		ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
 
-		// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
 		if (show_test_window)
 		{
 			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
