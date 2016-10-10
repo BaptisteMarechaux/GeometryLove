e7eb8a810df99336126f10c817cd533d00eb5c40
Manon Maillard <ravonyxa@gmail.com>
2016-10-10 11:25:25 +0200

    math function

--- GeomertyLove/Math.h
+++ GeomertyLove/Math.h
@@ -3,3 +3,7 @@
 
 int orientation(Point p, Point q, Point r);
 std::vector<Point> jarvisMarch(std::vector<Point> points);
+
+Point makeVector(Point p1, Point p2); 
+
+int normVector(Point p1, Point p2);
